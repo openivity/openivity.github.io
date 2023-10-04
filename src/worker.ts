21d7cf01812556bd5170ace88ae40d94689ebeca
Hari Mukti <muktihaz@gmail.com>
2023-10-04 21:22:59 +0700

    fix(worker.ts): wasm url (#7)

--- src/worker.ts
+++ src/worker.ts
@@ -2,7 +2,7 @@ import '@/assets/wasm/wasm_exec.js'
 
 const go = new Go()
 
-const wasmUrl = '/wasm/fitsvc.wasm'
+const wasmUrl = new URL('/wasm/fitsvc.wasm', import.meta.url)
 const wasm = WebAssembly.instantiateStreaming(fetch(wasmUrl), go.importObject).then((wasm) => {
   go.run(wasm.instance)
 })
