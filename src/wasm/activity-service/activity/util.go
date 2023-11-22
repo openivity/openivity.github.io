a190407671cec8d21f389020337934578a73a41e
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2023-11-22 23:19:50 +0700

    feat: change unknown sport handling behavior

--- src/wasm/activity-service/activity/util.go
+++ src/wasm/activity-service/activity/util.go
@@ -14,6 +14,8 @@ func IsConsideredMoving(sport string, speed *float64) bool {
 	}
 
 	switch sport {
+	case "Other", SportUnknown:
+		fallthrough // Since we don't know the specific sport, let's assume the slowest possible speed as our tolerance.
 	case "Hiking", "Walking", "Swimming": // slow moving exercise
 		return *speed > 0.1388 // = 0.5km/h
 	case "Running":
@@ -26,6 +28,10 @@ func IsConsideredMoving(sport string, speed *float64) bool {
 func HasPace(sport string) bool {
 	switch sport {
 	case "Hiking", "Walking", "Running", "Swimming":
+		return true
+	case "Other", SportUnknown:
+		// Since we don't know the specific sport, let's calculate the pace for now and allow the user to decide for themselves.
+		// It's better to provide additional information than to have none. (TBD)
 		return true
 	default:
 		return false
