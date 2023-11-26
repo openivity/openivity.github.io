af0bad91b8703fa6aee21edd06a88fec5fd227e8
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2023-11-26 21:13:25 +0700

    feat(FE): implement building block for encoding

--- src/workers/activity-service.ts
+++ src/workers/activity-service.ts
@@ -2,7 +2,42 @@ import { activityService } from '@/workers/wasm-services'
 
 onmessage = async (e) => {
   await activityService
-  // @ts-ignore
-  const result = decode(e.data)
-  postMessage(result)
+
+  const begin = new Date()
+
+  switch (e.data.type) {
+    case 'isReady':
+      postMessage({ type: e.data.type })
+      break
+    case 'decode': {
+      // @ts-ignore
+      const result = decode(e.data.input)
+      postMessage({
+        type: e.data.type,
+        result: result,
+        elapsed: new Date().getTime() - begin.getTime()
+      })
+      break
+    }
+    case 'encode': {
+      // @ts-ignore
+      const result = encode(e.data.input)
+      postMessage({
+        type: e.data.type,
+        result: result,
+        elapsed: new Date().getTime() - begin.getTime()
+      })
+      break
+    }
+    case 'manufacturerList': {
+      // @ts-ignore
+      const manufacturers = manufacturerList(e.data.input)
+      postMessage({
+        type: e.data.type,
+        result: manufacturers,
+        elapsed: new Date().getTime() - begin.getTime()
+      })
+      break
+    }
+  }
 }
