fb535a8d55c255e5b94712b25195a6e17e0acef8
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2023-12-02 13:03:52 +0700

    add missing field in class Sport in typescript

--- src/workers/activity-service.ts
+++ src/workers/activity-service.ts
@@ -44,6 +44,7 @@ onmessage = async (e) => {
     case 'sportList': {
       // @ts-ignore
       const sports = sportList(e.data.input)
+      console.log(sports)
       postMessage({
         type: e.data.type,
         result: sports,
