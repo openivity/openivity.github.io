a349a106ffa5f1860c626fe1b345f93f77c733f3
Hikmatulloh Hari Mukti <muktihaz@gmail.com>
2023-12-13 13:03:45 +0700

    chore: update help text

--- src/components/ToolFileTypeSelector.vue
+++ src/components/ToolFileTypeSelector.vue
@@ -4,7 +4,7 @@ import type { PropType } from 'vue'
 </script>
 <template>
   <div>
-    <label>Target File Type</label>
+    <label class="pe-1">Target File Type</label>
     <div class="col-12 pt-1">
       <v-select
         label="label"
@@ -18,19 +18,68 @@ import type { PropType } from 'vue'
       </v-select>
       <div class="pt-1">
         <p v-show="selected.value == FileType.FIT">
-          FIT is currently the most advanced file format for storing activity data.
+          FIT, developed by Garmin, is currently the most advanced file format for storing activity
+          data. We strives to comply with the FIT Activity File (FIT_FILE_TYPE = 4) as defined by
+          <a href="https://developer.garmin.com/fit" target="_blank" rel="noopener noreferrer"
+            >Garmin FIT</a
+          >
+          that being implemented by
+          <a href="https://github.com/muktihari/fit" target="_blank" rel="noopener noreferrer"
+            >FIT SDK for Go</a
+          >
+          .
         </p>
-        <p v-show="selected.value == FileType.GPX">
-          <strong>Note: GPX does not support the 'Power' data field.</strong>
-        </p>
-        <p v-show="selected.value == FileType.TCX">
-          <strong>Note: GPX does not support the 'Temperature' data field.</strong>
-        </p>
-        <p v-show="selected.value != FileType.Unsupported && selected.value != FileType.FIT">
-          If your target platform is Strava, we recommend choosing FIT instead. Strava have shifted
-          their support to FIT files. If you choose GPX or TCX, the target device name may not map
-          correctly with Strava's device mapping database.
-        </p>
+        <div v-show="selected.value == FileType.GPX">
+          <p>
+            GPX is a widely used XML format for geospacial data developed by Topografix. We follow
+            <a
+              href="https://www.topografix.com/gpx/1/1/gpx.xsd"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Schema V1.1</a
+            >
+            with
+            <a
+              href="https://www8.garmin.com/xmlschemas/TrackPointExtensionv2.xsd"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Garmin Trackpoint Extension V2</a
+            >
+            which does not support the <strong>Power</strong> data field.
+          </p>
+        </div>
+        <div class="pt-1" v-show="selected.value == FileType.TCX">
+          <p>
+            The advantage of TCX is its ability to include <strong>Power</strong> information.
+            However, it does not support the <strong>Temperature</strong> data field. We follow
+            <a
+              href="https://www8.garmin.com/xmlschemas/TrainingCenterDatabasev2.xsd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Schema V2
+            </a>
+            with
+            <a
+              href="https://www8.garmin.com/xmlschemas/ActivityExtensionv2.xsd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Garmin Activity Extension V2
+            </a>
+            .
+          </p>
+        </div>
+        <div
+          class="pt-2"
+          v-show="selected.value != FileType.Unsupported && selected.value != FileType.FIT"
+        >
+          <p>
+            Note: If your target platform is Strava, we recommend choosing FIT instead. Strava have
+            shifted their support to FIT files. If you choose GPX or TCX, the target device name may
+            not map correctly with Strava's device mapping database.
+          </p>
+        </div>
       </div>
     </div>
   </div>
