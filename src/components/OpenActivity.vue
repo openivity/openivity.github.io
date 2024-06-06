ab61fafd10bb0fd5a86a6cb8f8b57cdf10ef8d37
Radityo <radityohernanda@gmail.com>
2024-06-06 18:40:39 +0700

    implement: new logo. update: change header web OpenActivity with svg.

--- src/components/OpenActivity.vue
+++ src/components/OpenActivity.vue
@@ -55,8 +55,7 @@ import SplitPaceGraph from './SplitPaceGraph.vue'
             <div :class="[isActivityFileReady ? 'default-border-top' : '']">
               <div class="pt-4 pb-2">
                 <h5 class="title">
-                  <img src="@/assets/logo.svg" width="32" />
-                  Open Activity
+                  <img src="@/assets/openivity-header.svg" />
                 </h5>
                 <div style="font-size: 0.9em">
                   Your data stays in your computer: 100% client-side power.
@@ -933,6 +932,10 @@ export default {
 </script>
 
 <style scoped>
+.title img {
+  height: 1.5em;
+}
+
 .v-enter-active,
 .v-leave-active {
   transition: opacity 100ms ease;
